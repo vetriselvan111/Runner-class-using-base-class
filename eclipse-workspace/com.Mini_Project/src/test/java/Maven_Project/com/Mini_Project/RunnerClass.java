47b523a7fc46bf11b403d1986d392a8bf7ecced7
vetriselvan111 <talentedvetri@gmail.com>
2021-12-07 20:38:21 +0530

    Runner class using POM

--- eclipse-workspace/com.Mini_Project/src/test/java/Maven_Project/com/Mini_Project/RunnerClass.java
+++ eclipse-workspace/com.Mini_Project/src/test/java/Maven_Project/com/Mini_Project/RunnerClass.java
@@ -7,91 +7,100 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.Mini_Project.Base_class;
+
+import pom.Home_Page;
+import pom.Login_Page;
+import pom.Page1;
+import pom.Page2;
+import pom.Page3;
+import pom.Page4;
+import pom.Page5;
+import pom.Page6;
+import pom.Page7;
+import pom.Page8;
+import pom.Page9;
+
 
 
 public class RunnerClass extends Base_class {
 	
-	public static WebDriver driver;
+	public static WebDriver driver=getBrowser("Chrome");
+	public static Home_Page hp = new Home_Page(driver);
+	public static Login_Page Login = new Login_Page(driver);
+	public static Page1 pg1 = new Page1(driver);
+	public static Page2 pg2 = new Page2(driver);
+	public static Page3 pg3 = new Page3(driver);
+	public static Page4 pg4 = new Page4(driver);
+	public static Page5 pg5 = new Page5(driver);
+	public static Page6 pg6 = new Page6(driver);
+	public static Page7 pg7 = new Page7(driver);
+	public static Page8 pg8 = new Page8(driver);
+	public static Page9 pg9 = new Page9(driver);
 	
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
-		driver = getBrowser("Chrome");
-		
 		URL("http://automationpractice.com/index.php");
 		
-		WebElement Login = driver.findElement(By.xpath("//a[@class='login']"));
+		clickOnElement(hp.getLogin());
 		
-		clickOnElement(Login);
-		
-		WebElement Email = driver.findElement(By.xpath("(//input[@name='email'][1])"));
-		
-		sendKeysElement(Email, "talentedvetri@gmail.com");
-		
-		WebElement Password = driver.findElement(By.xpath("(//input[@name='passwd'][1])"));
+		sendKeysElement(Login.getEmail(), "talentedvetri@gmail.com");
 		 
-		sendKeysElement(Password, "Mithun@123");
+		sendKeysElement(Login.getPassword(), "Mithun@123");
 		
-		WebElement Submit_login = driver.findElement(By.id("SubmitLogin"));
+		clickOnElement(Login.getSubmit_login());
 		
-		clickOnElement(Submit_login);
+		clickOnElement(pg1.getTshirt());
 		
-		clickOnElement(driver.findElement(By.xpath("(//a[text()='T-shirts'])[2]")));
-				
-		actionsPerform("moveToElement", driver.findElement(By.xpath("//img[@title='Faded Short Sleeve T-shirts']")));
+		actionsPerform("moveToElement", pg2.getImage());
 		
 		implicityWait(5, TimeUnit.SECONDS);
 		
-		actionsPerform("click", driver.findElement(By.xpath("//a[@class='quick-view']")));
+		actionsPerform("click", pg2.getImg_click());
+	
+		frames("element", null, pg3.getFrame());
 		
-		implicityWait(5, TimeUnit.SECONDS);
-		
-		WebElement addtocart = driver.findElement(By.xpath("//iframe[@vspace='0']"));
-		
-		frames("element", null, addtocart);
-		
-		clickOnElement(driver.findElement(By.xpath("//button[@class='exclusive']")));
+		clickOnElement(pg3.getAddtocart());
 		
 		frames("defaultContent", null, null);
 		
-		explicityWait("visibilityOfElement", 20, (By.xpath("//a[@title='Proceed to checkout']")));
+		explicityWait("visibilityOfElement", 20,pg4.getPtcheckout());
 		
-		WebElement ptcheckout = driver.findElement(By.xpath("//a[@title='Proceed to checkout']"));
+		actionsPerform("moveToElement",pg4.getPtcheckout());
 		
-		actionsPerform("moveToElement",ptcheckout);
-		
-		actionsPerform("click", ptcheckout);
+		actionsPerform("click", pg4.getPtcheckout());
 		
         scroll("scrollBy", "window.scrollBy(0,500);", null);
         
         implicityWait(5, TimeUnit.SECONDS);
 		
-		clickOnElement(driver.findElement(By.xpath("(//a[@title='Proceed to checkout'])[2]")));
+		clickOnElement(pg5.getPtcheckout1());
 		
 	    scroll("scrollBy", "window.scrollBy(0,500);", null);
 	    
 	    implicityWait(5, TimeUnit.SECONDS);
 	    
-	    clickOnElement(driver.findElement(By.xpath("(//button[@type='submit'])[2]")));
+	    clickOnElement(pg6.getPtcheckout2());
 	    
 	    implicityWait(5, TimeUnit.SECONDS);
 	    
-	    clickOnElement(driver.findElement(By.id("cgv")));
+	    clickOnElement(pg7.getCheckbox());
 	    
-	    actionsPerform("click", driver.findElement(By.xpath("//button[@name='processCarrier']")));
+	    actionsPerform("click", pg7.getPtcheckout3());
 	    
 	    scroll("scrollBy", "window.scrollBy(0,500);", null);
 	    
-	    actionsPerform("click", driver.findElement(By.xpath("//a[@class='cheque']")));
+	    actionsPerform("click", pg8.getCheque());
 	    
 	    scroll("scrollBy", "window.scrollBy(0,300);", null);
 	    
-	    actionsPerform("click", driver.findElement(By.xpath("//span[text()='I confirm my order']")));
+	    actionsPerform("click", pg9.getConfirm());
 	    
 	    scroll("scrollBy", "window.scrollBy(0,300);", null);
 	    
 	    implicityWait(5, TimeUnit.SECONDS);
 	    
-	    screenShot("./Screenshots/project5.png");
+	    screenShot("./Screenshots/project6.png");
 	    	    
 	}
 }
