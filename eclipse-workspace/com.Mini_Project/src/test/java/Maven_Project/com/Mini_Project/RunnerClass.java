b0c8351d3f4b53cc0010b3585bbdb005581c6991
vetriselvan111 <talentedvetri@gmail.com>
2021-12-11 15:54:56 +0530

    Runner class using PO MANAGER

--- eclipse-workspace/com.Mini_Project/src/test/java/Maven_Project/com/Mini_Project/RunnerClass.java
+++ eclipse-workspace/com.Mini_Project/src/test/java/Maven_Project/com/Mini_Project/RunnerClass.java
@@ -20,87 +20,79 @@ import pom.Page6;
 import pom.Page7;
 import pom.Page8;
 import pom.Page9;
+import sdp.Page_Object_Manager;
 
 
 
 public class RunnerClass extends Base_class {
 	
 	public static WebDriver driver=getBrowser("Chrome");
-	public static Home_Page hp = new Home_Page(driver);
-	public static Login_Page Login = new Login_Page(driver);
-	public static Page1 pg1 = new Page1(driver);
-	public static Page2 pg2 = new Page2(driver);
-	public static Page3 pg3 = new Page3(driver);
-	public static Page4 pg4 = new Page4(driver);
-	public static Page5 pg5 = new Page5(driver);
-	public static Page6 pg6 = new Page6(driver);
-	public static Page7 pg7 = new Page7(driver);
-	public static Page8 pg8 = new Page8(driver);
-	public static Page9 pg9 = new Page9(driver);
+	
+	public static Page_Object_Manager pom = new Page_Object_Manager(driver);
 	
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
 		URL("http://automationpractice.com/index.php");
 		
-		clickOnElement(hp.getLogin());
+		clickOnElement(pom.getInstanthp().getLogin());
 		
-		sendKeysElement(Login.getEmail(), "talentedvetri@gmail.com");
+		sendKeysElement(pom.getLogin().getEmail(), "talentedvetri@gmail.com");
 		 
-		sendKeysElement(Login.getPassword(), "Mithun@123");
+		sendKeysElement(pom.getLogin().getPassword(), "Mithun@123");
 		
-		clickOnElement(Login.getSubmit_login());
+		clickOnElement(pom.getLogin().getSubmit_login());
 		
-		clickOnElement(pg1.getTshirt());
+		clickOnElement(pom.getPg1().getTshirt());
 		
-		actionsPerform("moveToElement", pg2.getImage());
+		actionsPerform("moveToElement", pom.getPg2().getImage());
 		
 		implicityWait(5, TimeUnit.SECONDS);
 		
-		actionsPerform("click", pg2.getImg_click());
+		actionsPerform("click", pom.getPg2().getImg_click());
 	
-		frames("element", null, pg3.getFrame());
+		frames("element", null, pom.getPg3().getFrame());
 		
-		clickOnElement(pg3.getAddtocart());
+		clickOnElement(pom.getPg3().getAddtocart());
 		
 		frames("defaultContent", null, null);
 		
-		explicityWait("visibilityOfElement", 20,pg4.getPtcheckout());
+		explicityWait("visibilityOfElement", 20, pom.getPg4().getPtcheckout());
 		
-		actionsPerform("moveToElement",pg4.getPtcheckout());
+		actionsPerform("moveToElement",pom.getPg4().getPtcheckout());
 		
-		actionsPerform("click", pg4.getPtcheckout());
+		actionsPerform("click", pom.getPg4().getPtcheckout());
 		
         scroll("scrollBy", "window.scrollBy(0,500);", null);
         
         implicityWait(5, TimeUnit.SECONDS);
 		
-		clickOnElement(pg5.getPtcheckout1());
+		clickOnElement(pom.getPg5().getPtcheckout1());
 		
 	    scroll("scrollBy", "window.scrollBy(0,500);", null);
 	    
 	    implicityWait(5, TimeUnit.SECONDS);
 	    
-	    clickOnElement(pg6.getPtcheckout2());
+	    clickOnElement(pom.getPg6().getPtcheckout2());
 	    
 	    implicityWait(5, TimeUnit.SECONDS);
 	    
-	    clickOnElement(pg7.getCheckbox());
+	    clickOnElement(pom.getPg7().getCheckbox());
 	    
-	    actionsPerform("click", pg7.getPtcheckout3());
+	    actionsPerform("click", pom.getPg7().getPtcheckout3());
 	    
 	    scroll("scrollBy", "window.scrollBy(0,500);", null);
 	    
-	    actionsPerform("click", pg8.getCheque());
+	    actionsPerform("click", pom.getPg8().getCheque());
 	    
 	    scroll("scrollBy", "window.scrollBy(0,300);", null);
 	    
-	    actionsPerform("click", pg9.getConfirm());
+	    actionsPerform("click", pom.getPg9().getConfirm());
 	    
 	    scroll("scrollBy", "window.scrollBy(0,300);", null);
 	    
 	    implicityWait(5, TimeUnit.SECONDS);
 	    
-	    screenShot("./Screenshots/project6.png");
+	    screenShot("./Screenshots/project7.png");
 	    	    
 	}
 }
